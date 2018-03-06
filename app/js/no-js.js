fe94b5f43414dbc105996596c2c0f45f4b44e830
ttm <taras.tataryn.ttm@gmail.com>
2018-03-06 19:56:12 +0200

    Set reviews initial slider

--- app/js/no-js.js
+++ app/js/no-js.js
@@ -4,4 +4,10 @@
 	if (mainNav.classList.contains("main-nav--no-js")){
 		mainNav.classList.remove("main-nav--no-js");
 	}
+
+	if (document.querySelector(".reviews__slider").
+			classList.
+			contains("reviews__slider--no-js")){
+		document.querySelector(".reviews__slider").classList.remove("reviews__slider--no-js");
+	}
 })();
