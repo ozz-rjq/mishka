ef7206be20820d36cf96ee6609e3f9a62287a04a
ttm <taras.tataryn.ttm@gmail.com>
2018-03-16 16:12:52 +0200

    Fix no-js script

--- app/js/no-js.js
+++ app/js/no-js.js
@@ -1,15 +1,25 @@
 ;(function(){
 	var mainNav = document.querySelector(".main-nav");
+	var reviewsSlider = document.querySelector(".reviews__slider");
+	var reviewsBlock = document.querySelector(".reviews");
 
 	if (mainNav.classList.contains("main-nav--no-js")){
 		mainNav.classList.remove("main-nav--no-js");
 	}
 
-	if (document.querySelector(".reviews__slider")){
-		if (document.querySelector(".reviews__slider").
+	if (reviewsSlider){
+		if (reviewsSlider.
 				classList.
 				contains("reviews__slider--no-js")){
-			document.querySelector(".reviews__slider").classList.remove("reviews__slider--no-js");
+			reviewsSlider.classList.remove("reviews__slider--no-js");
+		}
+	}
+
+	if (reviewsBlock){
+		if (reviewsBlock.
+				classList.
+				contains("reviews--no-js")){
+			reviewsBlock.classList.remove("reviews--no-js");
 		}
 	}
 })();
