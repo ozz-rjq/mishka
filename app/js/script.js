6906c90a9144a259317d00058a2b6033c75f7268
ttm <taras.tataryn.ttm@gmail.com>
2018-03-20 15:08:18 +0200

    Change forEach loop for better capability

--- app/js/script.js
+++ app/js/script.js
@@ -2,24 +2,22 @@
 	var navToggle = document.querySelector(".main-nav__toggle");
 	var hiddenOptions = document.querySelectorAll(".main-nav__item--hidden");
 
-
 	navToggle.addEventListener("click", function(){
 		console.log(hiddenOptions);
 		if (navToggle.classList.contains("main-nav__toggle--open")){
 			navToggle.classList.remove("main-nav__toggle--open");
 			navToggle.classList.add("main-nav__toggle--close");
 
-			hiddenOptions.forEach(function(el){
-				el.classList.remove("main-nav__item--hidden");
-			});
+			for (var i = 0; i < hiddenOptions.length; i++){
+				hiddenOptions[i].classList.remove("main-nav__item--hidden");
+			}
 		} else{
 			navToggle.classList.add("main-nav__toggle--open");
 			navToggle.classList.remove("main-nav__toggle--close");
 
-			hiddenOptions.forEach(function(el){
-				el.classList.add("main-nav__item--hidden");
-			});
+			for (var i = 0; i < hiddenOptions.length; i++){
+				hiddenOptions[i].classList.add("main-nav__item--hidden");
+			}
 		}
 	});
-
 })();
